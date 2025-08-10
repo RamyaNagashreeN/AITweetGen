fe10580f12fad4d257ee54dca7160c22469ad469
RamyaNagashreeN <ramya.nagashree2@gmail.com>
2025-08-10 22:19:07 +0530

    Update Main.py
    Comments

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-#backend code to use Google Gemini model for generating tweets
+#Backend code to use Google Gemini for generating tweets
 import os
 
 os.environ['GOOGLE_API_KEY']  = st.secrets['GOOGLE_API_KEY']
 
-#Using Google Models (Gemini Pro)
+#Using Google Models
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain import LLMChain
 from langchain import PromptTemplate
@@ -20,18 +20,7 @@ gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 tweet_chain=tweet_template | gemini_model
 
 
-
-# Example of using the Gemini model
-#response = gemini_model.invoke("Give me 3 tweets on World War 1")
-
-# Display the output
-#print(response.content)
-
-
-
-
-
-#frontend code
+#Frontend code
 import streamlit as st
 
 # Custom theme configuration
@@ -62,26 +51,19 @@ st.title("AI Tweet Generator 🐦")
 st.markdown("""
     <h2 style='color: #1DA1F2;'>Generate Tweets on Any Topic</h2>
     """, unsafe_allow_html=True)
-
-#st.header("Generate Tweets on Any Topic")
-
-#st.subheader("Enter a topic below:")    
+ 
 
 topic = st.text_input("Topic")
 
 number_of_tweets = st.number_input("Number of Tweets", min_value=1, max_value=10, value=1, step=1)
 
-# if st.button("Generate Tweets"):
-#     st.write("Tweets will be generated here...")
-#     tweets=tweet_chain.invoke({"topic": topic, "number_of_tweets": number_of_tweets})
-#     st.write(tweets.content)
 
 if st.button("Generate Tweets"):
     with st.container():
         st.write("Tweets will be generated here...")
         tweets = tweet_chain.invoke({"topic": topic, "number_of_tweets": number_of_tweets})
-        #st.markdown(f"<div style='background-color: #f8f9fa; padding: 20px; border-radius: 10px;'>{tweets.content}</div>", 
-                  # unsafe_allow_html=True)
+        st.markdown(f"<div style='background-color: #f8f9fa; padding: 20px; border-radius: 10px;'>{tweets.content}</div>", 
+                  unsafe_allow_html=True)
 
 
 
